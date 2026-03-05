d2e23aef1174ce1fc10e583d678b36884f3156aa
wasun <Chanchakornj@gmail.com>
2026-03-06 02:25:34 +0700

    add new .yml file

--- src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
@@ -7,14 +7,14 @@ import org.springframework.stereotype.Service;
 public class GradeServiceImpl implements GradeService {
     @Override
     public String getGrade(double score) {
-        if (score > 79.5) {
+        if (score > 85) {
             return "A";
         }
-        else if (score > 74.5){
+        else if (score > 80){
             return "B";
-        }else if (score > 59.5){
+        }else if (score > 75){
             return "C";
-        }else if (score > 32.5) {
+        }else if (score > 40) {
             return "D";
         } else
             return "F";
